b82658b61604436b9504e2a6bf335b7b50219c9a
Ryan Gannon <ryanmgannon.dev@gmail.com>
2017-04-15 20:36:32 -0400

    Oh right, it was the printf format string that was wrong

--- 8/c/digits.c
+++ 8/c/digits.c
@@ -9,41 +9,41 @@
 
 int main( int argc, char * argv )
 {
-	// Create a buffer for 1000 digits, feed the contents of Problem8.txt 
+	// Create a buffer for 1000 digits, feed the contents of digits.txt 
 	// into them.
 	FILE *numbers_file = fopen("digits.txt", "r");
+	int input; 
+	unsigned int index = 0, digits[1000];
+	unsigned long long max = 0;
+
 
 	if (!numbers_file) {
-		printf("Either the file's missing or it returned null for some\
-			reason.  either way, it's not open.");
 		exit(EXIT_FAILURE);
 	}
 
-	int index = 0, max = 0, digits[1000];
-	char input;
-
 	while ((input = fgetc(numbers_file)) != EOF) {
-		printf("%d\n", atoi(&input));
 		if (input != '\n' && index < 1000) {
-			digits[index] = atoi(&input);
+			digits[index] = atoi((const char *) &input);
 			index++;
 		}
 	}
 
 	index = 0;
 	while (index + 12 <= 1000) {
-		unsigned long long int cur = 1;
+		unsigned long long cur = 1;
+
 		for (int i = index; i <= (index + 12); i++) {
 			cur *= digits[i];
 		}
 
-		if (cur > max)
+		if (cur > max) {
 			max = cur;
+		}
 
 		index++;
 	}
 
 	fclose(numbers_file);
 
-	printf("%d\n", max);
+	printf("%llu\n", max);
 }
