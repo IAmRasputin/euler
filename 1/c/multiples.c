14fd2eaddfd91a23d695e87fb08e83d3559f69c3
Ryan Gannon <ryanmgannon@gmail.com>
2025-11-26 15:53:30 -0500

    c re-do: day 1

--- 1/c/multiples.c
+++ 1/c/multiples.c
@@ -4,13 +4,10 @@ int main()
 {
 	int sum = 0;
 
-	int i;
-
-	for (i = 1; i < 1000; i++){
-		if (i % 3 == 0)
+	for (int i = 1; i < 1000; i++){
+		if (i % 3 == 0 || i % 5 == 0) {
 			sum += i;
-		else if (i % 5 == 0)
-			sum += i;
+		}
 	}
 	
 	printf("%d\n", sum);
